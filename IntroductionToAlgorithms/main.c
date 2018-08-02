438d0a094a93768ddd363720bc137cf1c35b98d3
lloveprt <donghao@11space.com>
2018-08-02 14:41:06 +0800

    添加

--- IntroductionToAlgorithms/main.c
+++ IntroductionToAlgorithms/main.c
@@ -8,8 +8,27 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int i, j, k;
+    for(i=1; i<n; i++)
+    {
+        //找到要插入的位置
+        for(j=0; j<i; j++)
+            if(a[i] < a[j])
+                break;
+        //插入，并后移剩余元素
+        if(j != i)
+        {
+            int temp = a[i];
+            for(int k=i-1; k>=j; k--)
+                a[k+1] = a[k];
+            a[j] = temp;
+        }
+    }
+    PrintDataArray(a, n);
     return 0;
+    
+    
+    
     
 }
