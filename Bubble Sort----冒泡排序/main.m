92d934bb71202eddaaf656faec233419d232040e
Affee <20198886+Affee@users.noreply.github.com>
2019-02-14 19:16:42 +0800

    修改

--- Bubble Sort----冒泡排序/main.m
+++ Bubble Sort----冒泡排序/main.m
@@ -10,21 +10,34 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSMutableArray *numbers = [NSMutableArray arrayWithObjects:@"2",@"312",@"42",@"124",@"66",@"27",@"97",@"82", nil];
-        NSLog(@"排序前%@",numbers);
-        for (int i = 0; i < numbers.count ; i++) {
-//            比较的趟数
-            for (int j = 0; j < numbers.count - i-1; j++) {
-//                比较的次数
-                if ([numbers[j] intValue] > [numbers[j+1] intValue] ) {
-//                    升序排序
-                    int temp = [numbers[j] intValue ];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = [NSString stringWithFormat:@"%d",temp];
+//        NSMutableArray *numbers = [NSMutableArray arrayWithObjects:@"2",@"312",@"42",@"124",@"66",@"27",@"97",@"82", nil];
+//        NSLog(@"排序前%@",numbers);
+//        for (int i = 0; i < numbers.count ; i++) {
+////            比较的趟数
+//            for (int j = 0; j < numbers.count - i-1; j++) {
+////                比较的次数
+//                if ([numbers[j] intValue] > [numbers[j+1] intValue] ) {
+////                    升序排序
+//                    int temp = [numbers[j] intValue ];
+//                    numbers[j] = numbers[j+1];
+//                    numbers[j+1] = [NSString stringWithFormat:@"%d",temp];
+//                }
+//            }
+//        }
+//        NSLog(@"排序后%@",numbers);
+        NSMutableArray *result = [NSMutableArray arrayWithObjects:@"2",@"312",@"42",@"124",@"66",@"27",@"97",@"82", nil];
+        //        NSLog(@"排序前%@",numbers);
+        for (int i = 0; i<result.count-1; i++) {
+            for (int j = 0; j<result.count-1-i; j++) {
+                NSInteger left = [result[j] integerValue];
+                NSInteger right = [result[j+1] integerValue];
+                if (left>right) {
+                    [result exchangeObjectAtIndex:j withObjectAtIndex:j+1];
                 }
             }
         }
-        NSLog(@"排序后%@",numbers);
+        NSLog(@"%@",result);
+    
     }
     return 0;
 }
