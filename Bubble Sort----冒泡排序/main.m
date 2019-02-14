0f31f416fb25e25a8654016a6ae6814a5a8762d7
Affee <20198886+Affee@users.noreply.github.com>
2019-02-14 19:03:07 +0800

    冒泡排序

---
+++ Bubble Sort----冒泡排序/main.m
@@ -0,0 +1,30 @@
+//
+//  main.m
+//  Bubble Sort----冒泡排序
+//
+//  Created by affee on 14/02/2019.
+//  Copyright © 2019 affee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSMutableArray *numbers = [NSMutableArray arrayWithObjects:@"2",@"312",@"42",@"124",@"66",@"27",@"97",@"82", nil];
+        NSLog(@"排序前%@",numbers);
+        for (int i = 0; i < numbers.count ; i++) {
+//            比较的趟数
+            for (int j = 0; j < numbers.count - i-1; j++) {
+//                比较的次数
+                if ([numbers[j] intValue] > [numbers[j+1] intValue] ) {
+//                    升序排序
+                    int temp = [numbers[j] intValue ];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = [NSString stringWithFormat:@"%d",temp];
+                }
+            }
+        }
+        NSLog(@"排序后%@",numbers);
+    }
+    return 0;
+}
